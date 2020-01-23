5a5c14677b5a316ddeea7585fad0ed0fd860444c
Ashok Bayya <ashokbayya531@gmail.com>
2020-01-23 17:26:02 +0530

    Add files via upload

---
+++ android/app/src/main/java/com/guessing_game_reloaded/MainActivity.java
@@ -0,0 +1,28 @@
+package com.guessing_game_reloaded;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "guessing_game_reloaded";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
+}
